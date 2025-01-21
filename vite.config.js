97fd4894ab53cb0186522c68ddf9ca8c468a0fa5
SamuelS2003 <samuelsholademi37@gmail.com>
2025-01-21 10:35:07 +0100

    add

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/SamuelS2003/Portfolio-Samuel-.git",
+  build: {
+    outDir: "dist",
   plugins: [react()],
 })
